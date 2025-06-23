6c3dec9fc0d84b765a75c36fcfe6bcaac7995d95
itsmayanksingh <itsmayanksingh1@gmail.com>
2025-06-23 17:26:39 +0530

    Auction system added

---
+++ src/app/dashboard/my-bids/page.jsx
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react'
+import MyBids from '@/components/PagesComponent/Dashboard/MyBids'
+
+export default function MyBidsPage() {
+  return (
+    <div className="container">
+      <MyBids />
+    </div>
+  )
+}
